7907d1a45d5b007d815df7b0ff54e9ea2be31846
Crispin Wellington <retrogradeorbit@gmail.com>
2015-12-12 15:09:22 +0800

    asset definition

---
+++ src/ld34/assets.cljs
@@ -0,0 +1,50 @@
+(ns ld34.assets)
+
+(def assets
+  {:ground-1
+   {:pos [0 0]
+    :size [32 24]}
+
+   :ground-2
+   {:pos [32 0]
+    :size [16 16]}
+
+   :grass-1
+   {:pos [48 0]
+    :size [16 16]}
+
+   :grass-2
+   {:pos [64 0]
+    :size [16 16]}
+
+   :grass-3
+   {:pos [80 0]
+    :size [16 16]}
+
+   :tree-1
+   {:pos [31 56]
+    :size [9 16]}
+
+   :tree-2
+   {:pos [48 56]
+    :size [8 16]}
+
+   :tree-3
+   {:pos [56 64]
+    :size [8 8]}
+
+   :tree-4
+   {:pos [64 64]
+    :size [8 8]}
+
+   :tree-5
+   {:pos [72 56]
+    :size [8 16]}
+
+   :tree-6
+   {:pos [80 56]
+    :size [24 16]}
+
+
+   }
+  )
