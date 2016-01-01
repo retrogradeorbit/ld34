1719f0be87f22013547b20389ff17301b3e26195
Crispin Wellington <retrogradeorbit@gmail.com>
2016-01-01 18:31:29 +0800

    tree chopping code

--- src/ld34/tree.cljs
+++ src/ld34/tree.cljs
@@ -1,6 +1,13 @@
-(ns ld34.tree)
+(ns ld34.tree
+  (:require [infinitelives.pixi.sprite :as sprite]
+            [infinitelives.utils.sound :as sound]
+            [infinitelives.utils.events :as events]
+            [infinitelives.utils.vec2 :as vec2]
+            [cljs.core.async :refer [<! chan put!]]
+            [ld34.effects :as effects])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(defn update-tree-chop [game]
+(defn update-chop [game]
   (when (-> @game :walker :chopping)
     (let [frame (-> @game :walker :action-count)
           plant (-> @game :walker :chopping)
@@ -11,43 +18,45 @@
         (sprite/set-pos! (:sprite plant) (+ x intensity) y)
         (sprite/set-pos! (:sprite plant) (- x intensity) y)))))
 
-(defn chop-tree-go-thread [game {:keys [pos sprite id yield] :as tree}]
+(defn- tree-chopped-down [game canvas {:keys [yield sprite]} set-seed-text]
+  (sound/play-sound :tree-harvest 0.5 false)
+  (.removeChild (-> canvas :layer :world) sprite)
+
+  (swap! game
+         #(-> %
+              (assoc-in [:walker :action] :walk)
+              (update :plants disj (first (filter
+                                           (fn [pl]
+                                             (= (:sprite pl) sprite))
+                                           (:plants %))))
+              (update :dollars + (* yield (:plant-multiplier %)))
+              (update :seeds + (:max-seeds %))))
+  (set-seed-text (:seeds @game) (:dollars @game)))
+
+(defn- tree-chopping [game {:keys [sprite pos]}]
   (go
+    ;(sprite/set-pos! sprite pos)
+    (<! (events/wait-time 30))
+
+    (sound/play-sound :tree-chop 0.5 false)
+    (loop [f 0]
+      (when (< f (:chop-length @game))
+        (sprite/set-pos! sprite (+
+                                 (vec2/get-x pos)
+                                 (effects/shake 10 2 0.07 f))
+                         (vec2/get-y pos))
+        (<! (events/next-frame))
+        (recur (inc f))))))
+
+(defn chop-tree-go-thread [game canvas {:keys [pos sprite id yield] :as tree} set-seed-text]
+  (go
+    ;; tree chopping
     (loop [chop-num 0]
-      (if (> chop-num (:chop-num @game))
-        ;; tree chopped down
-        (do
-          (sound/play-sound sfx-tree-harvest  0.5 false)
-          (.removeChild (-> canvas :layer :world) sprite)
+      (when-not (= :walk (-> @game :walker :action))
+        (<! (tree-chopping game tree))
+        (when (< chop-num (:chop-num @game))
+          (recur (inc chop-num)))))
 
-          (swap! game
-                 #(-> %
-                      (assoc-in [:walker :action] :walk)
-                      (update :plants disj (first (filter
-                                                   (fn [pl]
-                                                     (= (:sprite pl) sprite))
-                                                   (:plants @game))))
-                      (update :dollars + (* yield (:plant-multiplier %)))
-                      (update :seeds + (:max-seeds %))))
-          (set-seed-text (:seeds @game) (:dollars @game)))
-
-        (do
-          (sprite/set-pos! sprite pos)
-          (<! (events/wait-time 30))
-
-          (if (= :walk (-> @game :walker :action))
-            ;; exit
-            nil
-
-            (do
-              ;; shake
-              (sound/play-sound sfx-tree-chop 0.5 false)
-              (loop [f 0]
-                (when (< f (:chop-length @game))
-                  (sprite/set-pos! sprite (+
-                                           (vec2/get-x pos)
-                                           (effects/shake 10 2 0.07 f))
-                                   (vec2/get-y pos))
-                  (<! (events/next-frame))
-                  (recur (inc f))))
-              (recur (inc chop-num)))))))))
+    ;; finished chopping down
+    (when-not (= :walk (-> @game :walker :action))
+      (tree-chopped-down game canvas tree set-seed-text))))
