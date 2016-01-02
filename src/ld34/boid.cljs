8edcc17f226055ff9b8a0f29ae1d2f340194f4c6
Crispin Wellington <retrogradeorbit@gmail.com>
2016-01-02 12:18:09 +0800

    add appropriation and cleanup

--- src/ld34/boid.cljs
+++ src/ld34/boid.cljs
@@ -1,10 +1,11 @@
 (ns ld34.boid
   (:require [infinitelives.utils.vec2 :as vec2]
             [infinitelives.utils.math :as math]
-            [infinitelives.utils.console :refer [log]])
-)
+            [infinitelives.utils.console :refer [log]]))
 
 ;; functions for boid like behavoir
+;; implementation of Craig Reynolds style boids
+;; ref: http://www.red3d.com/cwr/papers/1999/gdc99steer.html
 
 (def example
   {:mass 10.0
@@ -35,26 +36,17 @@
 (defn arrive [{:keys [mass pos vel max-force max-speed] :as boid} target slowing-distance]
   (let [target-offset (vec2/sub target pos)
         distance (+ 0.0001 (vec2/magnitude target-offset))
-        ;_ (log "distance" distance)
         ramped-speed (* max-speed (/ distance slowing-distance))
         clipped-speed (min ramped-speed max-speed)
-
         desired-vel (vec2/scale target-offset (/ clipped-speed distance))
-
-        ;_ (log "dv" desired-vel)
         steering (-> desired-vel
                      (vec2/sub vel)
                      (vec2/truncate max-force))
         accel (vec2/scale steering (/ 1 mass))
-        ;_ (log "acc" accel)
         new-vel (-> accel
                     (vec2/add vel)
                     (vec2/truncate max-speed))
-        ;_ (log "new" new-vel)
-
-        new-pos (vec2/add pos new-vel)
-        ;_ (log "new-pos" new-pos)
-        ]
+        new-pos (vec2/add pos new-vel)]
     (assoc boid
            :pos new-pos
            :vel new-vel)))
@@ -79,26 +71,21 @@
 
 (defn wander [{:keys [mass pos vel max-force max-speed last-steer]
                :as boid}
-              d r delta
-              ]
+              d r delta]
   (let [last-steer (or last-steer
                        ;; front of circle to initialise
                        (vec2/vec2 (+ d r) 0))
-
         delta-vec (-> (vec2/random-unit)
                       (vec2/scale delta))
-
         new-steer (-> last-steer
                       (vec2/add delta-vec)
                       (constrain (vec2/vec2 d 0) r))
-
         direction (if (vec2/almost (vec2/zero) vel)
                     (vec2/random-unit)
                     (vec2/unit vel))
 
         ;; rotate into frame of reference of boid heading
         rotated-new-steer (vec2/rotate new-steer (vec2/heading direction))
-
         steering (-> rotated-new-steer
                      (vec2/sub vel)
                      (vec2/truncate max-force))
@@ -106,9 +93,7 @@
         new-vel (-> accel
                     (vec2/add vel)
                     (vec2/truncate max-speed))
-        ;_ (log "new vel" new-vel)
-        new-pos (vec2/add pos new-vel)
-]
+        new-pos (vec2/add pos new-vel)]
     (assoc boid
            :pos new-pos
            :vel new-vel
